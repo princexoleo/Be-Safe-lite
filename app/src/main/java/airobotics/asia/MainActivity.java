01ad3cca109ace97d107ddc5e05ba9629013a590
maz_leo <xtleon.xl@gmail.com>
2019-05-26 03:27:16 +0600

    initialize views with XML

--- app/src/main/java/airobotics/asia/MainActivity.java
+++ app/src/main/java/airobotics/asia/MainActivity.java
@@ -4,14 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private static final String TAG = "MainActivity";
+
+    //
+    Toolbar toolbar;
+    EditText emailEditText, passwordEditText;
+    CheckBox checkBox;
+    ImageButton signinButton;
+    Button signupButton;
+    //
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize view components with XML files
+        toolbar = findViewById(R.id.toolbar);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        signinButton = findViewById(R.id.signin);
+        signupButton = findViewById(R.id.signup);
+
+        //
     }
 }
