74272e9515134dda159229dec5b2f76abb0184de
maz_leo <xtleon.xl@gmail.com>
2019-05-26 03:34:09 +0600

    button event listener handle
    added button event listener

--- app/src/main/java/airobotics/asia/MainActivity.java
+++ app/src/main/java/airobotics/asia/MainActivity.java
@@ -3,11 +3,15 @@ package airobotics.asia;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -35,5 +39,26 @@ public class MainActivity extends AppCompatActivity {
         checkBox = findViewById(R.id.checkbox);
 
         //
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if sign in button pressed ..
+                Log.d(TAG, "onClick: sign-in button");
+                Toast.makeText(MainActivity.this, "Sign in Button Pressed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sign up button pressed
+                Log.d(TAG, "onClick: sign-up button");
+               // Toast.makeText(MainActivity.this, "Sign Up Button Pressed!", Toast.LENGTH_SHORT).show();
+                //we are going to next activity so we need a intent
+                
+
+            }
+        });
     }
 }
