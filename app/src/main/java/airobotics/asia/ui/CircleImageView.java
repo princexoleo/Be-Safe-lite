1de336cd898d258cf3c234815b5d70577f7d2a4a
maz_leo <xtleon.xl@gmail.com>
2019-05-27 01:59:36 +0600

    package imported

--- app/src/main/java/airobotics/asia/ui/CircleImageView.java
+++ app/src/main/java/airobotics/asia/ui/CircleImageView.java
@@ -3,14 +3,23 @@ package airobotics.asia.ui;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+import android.view.MotionEvent;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.Dimension;
+import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
 
 import airobotics.asia.R;
