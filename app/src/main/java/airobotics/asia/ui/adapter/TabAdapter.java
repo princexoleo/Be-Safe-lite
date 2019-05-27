e4734c37e8784c68f56e70cda936b55a36c7e2c7
maz_leo <xtleon.xl@gmail.com>
2019-05-27 16:35:37 +0600

    v-24 support added also added some fragment

---
+++ app/src/main/java/airobotics/asia/ui/adapter/TabAdapter.java
@@ -0,0 +1,37 @@
+package airobotics.asia.ui.adapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+    private static final String TAG = "TabAdapter";
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitleList.get(position);
+    }
+}
