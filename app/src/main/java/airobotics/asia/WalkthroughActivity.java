c40ec7b82386f1fc77f361e24b646adf23a19664
maz_leo <xtleon.xl@gmail.com>
2019-05-31 19:25:28 +0600

    add walkthrough activity and drawable file
    add another activity called walk through to send a meesage to user about our application

---
+++ app/src/main/java/airobotics/asia/WalkthroughActivity.java
@@ -0,0 +1,54 @@
+package airobotics.asia;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
+
+public class WalkthroughActivity extends AppCompatActivity {
+
+    Button nextBtn,skipBtn;
+
+    private String [] permission ={Manifest.permission.SEND_SMS,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+    String rationale = "Please provide location permission so that you can access";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walkthrough);
+
+        nextBtn=findViewById(R.id.next_btn_id);
+        skipBtn=findViewById(R.id.skip_btn_id);
+
+        Permissions.Options options=new Permissions.Options()
+                .setRationaleDialogTitle("info")
+                .setSettingsDialogTitle("warning");
+
+
+
+        Permissions.check(WalkthroughActivity.this, permission, rationale, options, new PermissionHandler() {
+            @Override
+            public void onGranted() {
+                nextBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent goLogin=new Intent(WalkthroughActivity.this,LoginActivity.class);
+                        startActivity(goLogin);
+                        finish();
+                    }
+                });
+
+            }
+        });
+
+
+    }
+}
