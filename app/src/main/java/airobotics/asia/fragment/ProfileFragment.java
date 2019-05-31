c70a4e28d487e0d00a0466fd4aebdb2af7959486
maz_leo <xtleon.xl@gmail.com>
2019-05-31 21:50:47 +0600

    Update ProfileFragment.java

--- app/src/main/java/airobotics/asia/fragment/ProfileFragment.java
+++ app/src/main/java/airobotics/asia/fragment/ProfileFragment.java
@@ -271,7 +271,7 @@ public class ProfileFragment extends Fragment {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},100);
             return  true;
         }
-        return false;
+        return true;
 
     }
     
